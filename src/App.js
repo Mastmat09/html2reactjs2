231b98423200354b9368d8be2a25a2d82fe13596
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-03-27 18:24:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import './headers.css';
+import './features.css';
+import { Header } from "./components/Header";
+import { Inside } from "./components/Inside";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
