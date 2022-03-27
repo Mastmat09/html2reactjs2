3a3ba1b27470962902b03891e8485aca62b571b7
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-03-27 19:12:23 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,7 @@ export const Footer = () => {
       <section className="containerfoot navbar-expand-md fixed-bottom" >
       <footer className="text-muted py-1">
         <div className="container">
-          <p className="float-end mb-1">
-            <a href="#">Back to top</a>
+          <p className="float-end mb-1">            
           </p>
           <p className="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
           <p className="mb-0">New to Bootstrap? </p>
