72c02000e4d609303d47c71a97e777ec4363b5e0
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-04-02 18:42:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './headers.css';
 import './features.css';
 import { Header } from "./components/Header";
 import { Inside } from "./components/Inside";
+import { Inside1 } from "./components/Inside1";
 import { Footer } from "./components/Footer";
 
 function App() {
   return (   
     <>
        <Header />
-       <Inside />     
+       <Inside />  
+       <Inside1 />
        <Footer />  
    </>
   );
