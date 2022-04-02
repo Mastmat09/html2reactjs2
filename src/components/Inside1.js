443197a3b7957b73d070e81efaeb59d61d8fad30
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-04-02 18:41:26 +0200

    Add files via upload

---
+++ src/components/Inside1.js
@@ -0,0 +1,39 @@
+export const Inside1 = () => {
+    return (
+      
+<section>
+<div className="d-flex justify-content-center bgcolorhtmlreact ">
+  
+  <div className="mx-1 text-center overflow-hidden fifty50">
+    <div className="my-3 p-3">
+      <h2 className="display-5">HTML</h2>
+      <p className="lead">HyperText Markup Language</p>
+    </div>
+    <div className="bg-body shadow-sm mx-auto bgimagedesign" />
+  </div>
+
+  <div className="mx-1 text-center overflow-hidden fifty50">
+    <div className="my-3 py-3">
+      <h2 className="display-5">React JS</h2>
+      <p className="lead">JavaScript library for building user interfaces based on UI components</p>
+    </div>
+    <div className="bg-body shadow-sm mx-auto bgimagedesignreact" />
+  </div>
+
+</div>
+<div className="bg-dark text-secondary px-4 py-5 text-center">
+    <div className="py-5">
+      <h1 className="display-5 fw-bold text-white">Transform your website into React</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="fs-5 mb-4">The react new system technology allow customer to define their ways how to create a new component</p>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+          <button type="button" className="btn btn-outline-light btn-lg px-4">Secondary</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ </section>
+        );
+    };
