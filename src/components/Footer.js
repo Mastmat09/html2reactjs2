defd2d1321dabd6f015c4428b0b62512d2e0f43e
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-04-03 20:16:01 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,19 @@
 
 export const Footer = () => {
     return (
-      <section className="containerfoot navbar-expand-md fixed-bottom" >
-      <footer className="text-muted py-1">
-        <div className="container">
-          <p className="float-end mb-1">            
-          </p>
-          <p className="mb-1">&copy; Html2react / is a new service provided to transform your website into React JS</p>
-          <p className="mb-0">Need help? </p>
+         <div id="about" className="bg-dark text-secondary px-4 py-5 text-center">
+    <div className="py-5">
+      <h1 className="display-5 fw-bold text-white">Transform your website into React</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="fs-5 mb-4">The react new system technology allow customer to define their ways how to create a new component</p>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+          <button type="button" className="btn btn-outline-light btn-lg px-4">Secondary</button>
         </div>
-      </footer>
-    </section>
+      </div>
+    </div>
+      <p className="mb-1 text-center text-small">&copy; 2022 HTML2REACT is newly produced by <a href="http://www.webolmes.fr" >www.webolmes.fr</a></p>
+    
+    </div>
     );
   };
