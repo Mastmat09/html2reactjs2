95a4cd7247ceb611c692a7caac6b99a1db43fc52
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-04-03 16:06:52 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import Image from "react-bootstrap/Image";
 import Bootstrap from "../components/assets/bootstrap-themes.webp";
-
+import Logoreact from "../components/assets/covertrans.png";
 
 export const Header = () => {
   return (
@@ -12,7 +12,7 @@ export const Header = () => {
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
           <svg className="bi me-2" width="40" height="32"></svg>
-          <span className="fs-4">Html2React...</span>
+          <span className="fs-4"><Image src={Logoreact}  className="d-block mx-lg-auto img-fluid" alt="Logoreact" width="200" height="48" loading="lazy" /></span>
         </a>
         <ul className="nav nav-pills">
         <li className="nav-item"><a href="/" className="nav-link" aria-current="page">Home</a></li>
