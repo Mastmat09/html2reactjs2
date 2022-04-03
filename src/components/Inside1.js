d1c525c085f56f5ba2ec90d30947219172cc149f
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-04-03 20:15:21 +0200

    Update Inside1.js

--- src/components/Inside1.js
+++ src/components/Inside1.js
@@ -2,10 +2,8 @@
 export const Inside1 = () => {
     return (
       
-<section id="compare">
-      
-<div className="d-flex justify-content-center bgcolorhtmlreact ">
-  
+<section id="compare">      
+<div className="d-flex justify-content-center bgcolorhtmlreact ">  
   <div className="mx-1 text-center overflow-hidden fifty50">
     <div className="my-3 p-3">
       <h2 className="display-5">HTML</h2>
@@ -13,7 +11,6 @@ export const Inside1 = () => {
     </div>
     <div className="bg-body shadow-sm mx-auto bgimagedesign" />
   </div>
-
   <div className="mx-1 text-center overflow-hidden fifty50">
     <div className="my-3 py-3">
       <h2 className="display-5">React JS</h2>
@@ -21,21 +18,7 @@ export const Inside1 = () => {
     </div>
     <div className="bg-body shadow-sm mx-auto bgimagedesignreact" />
   </div>
-
-</div>
-<div className="bg-dark text-secondary px-4 py-5 text-center">
-    <div className="py-5">
-      <h1 className="display-5 fw-bold text-white">Transform your website into React</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="fs-5 mb-4">The react new system technology allow customer to define their ways how to create a new component</p>
-        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-          <button type="button" className="btn btn-outline-light btn-lg px-4">Secondary</button>
-        </div>
-      </div>
-    </div>
-  </div>
- 
- </section>
+</div> 
+</section>
         );
     };
