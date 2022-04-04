7ed3e5ead853cfd407705684da664b6b043ff7f2
Matthieu <100421924+Mastmat09@users.noreply.github.com>
2022-04-04 13:46:53 +0200

    Update Inside1.js

--- src/components/Inside1.js
+++ src/components/Inside1.js
@@ -4,14 +4,14 @@ export const Inside1 = () => {
       
 <section id="compare">      
 <div className="row justify-content-center bgcolorhtmlreact ">  
-  <div className="col-md-6 col-lg-6 mb-5 mb-md-0 text-center overflow-hidden fifty50">
+  <div className="col-md-6 col-lg-6 mb-5 mb-md-0 text-center overflow-hidden">
     <div className="my-3 p-3">
       <h2 className="display-5">HTML</h2>
       <p className="lead">HyperText Markup Language</p>
     </div>
     <div className="bg-body shadow-sm mx-auto bgimagedesign" />
   </div>
-  <div className="col-md-6 col-lg-6 mb-5 mb-md-0 text-center overflow-hidden fifty50">
+  <div className="col-md-6 col-lg-6 mb-5 mb-md-0 text-center overflow-hidden">
     <div className="my-3 py-3">
       <h2 className="display-5">React JS</h2>
       <p className="lead">JavaScript library for building user interfaces based on UI components</p>
